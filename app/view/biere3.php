256d9d751c903670a2c9af8906807764aa25e2e6
Charlotte1303 <144892844+Charlotte1303@users.noreply.github.com>
2024-05-23 16:47:24 +0200

    blabla

---
+++ app/view/biere3.php
@@ -0,0 +1,21 @@
+
+<main> 
+<div class="beer-description">
+  <h2>LAVANDE COSMIQUE</h2>
+  <p>Une bière qui vous fera voyager à travers un mélange d'agrumes et épices, avec des arômes de lavande.</p>
+  <p>Bière brune à la lavande d’inspiration belge non ﬁltrée non pasteurisée.</p>
+  <h3>Ingrédients:</h3>
+  <ul>
+    <li>Eau: 92,04%</li>
+    <li>Malts</li>
+    <li>Houblon: 0,17%</li>
+    <li>Levure: 0,04%</li>
+    <li>Lavande: 0,03%</li>
+    <li>Graines de coriandre: 0,05%</li>
+  </ul>
+  <p>Amertume: <span>(no value provided)</span></p>
+  <p>Coloration: <span>(no value provided)</span></p>
+  <p>Contient du gluten</p>
+</div>
+<img src="smartmockups_lt8umi7b.png" alt="Beer Portrait" class="portrait-image">
+</main>
