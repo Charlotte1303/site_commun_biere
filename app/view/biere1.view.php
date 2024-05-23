ba5c5462f12d79316883e38f94b49bbfd282881f
AlmanarEON <alexandre.lebon.ecole@gmail.com>
2024-05-23 16:51:16 +0200

    maj page biere individuelle

---
+++ app/view/biere1.view.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/biere.css">
+    <title>Nos bières</title>
+</head>
+<body>
+<header>
+        <img src="public/images/Logo.png">
+        <nav class="menu">
+            <ul>
+                <li>
+                    <a href="app/view/index.html">Accueil</a>
+                </li>
+                <li>
+                    <a href="app/view/page_a_propos.html">A propos</a>
+                </li>
+                <li>
+                    <a href="app/view/page_nos_bieres.html">Nos bières</a>
+                </li>
+                <li>
+                    <a href="app/view/nous_contacter.html">Contact</a>   
+                </li>
+
+            </ul>
+
+        </nav>
+    </header>
+<main> 
+<div class="beer-description">
+  <h2>SOUFFLE SICHUANAIS</h2>
+  <p>Une bière qui vous fera voyager à travers un mélange d'agrumes et épices, avec le poivre de Sichuan.</p>
+  <p>Bière blonde au poivre de Sichuan d'inspiration belge non filtrée non pasteurisée.</p>
+  <h3>Ingrédients:</h3>
+  <ul>
+    <li>Eau: 92,04%</li>
+    <li>Malts</li>
+    <li>Houblon: 0,17%</li>
+    <li>Levure: 0,04%</li>
+    <li>Poivre de Sichuan: 0,03%</li>
+    <li>Graines de coriandre: 0,05%</li>
+  </ul>
+  <p>Amertume: <span>(no value provided)</span></p>
+  <p>Coloration: <span>(no value provided)</span></p>
+  <p>Contient du gluten</p>
+</div>
+<img src="public/images/smartmockups_lt8udws5" alt="Beer Portrait" class="portrait-image">
+</main>
+<footer>
+        <p>© 2024 Infinité. Tous droits réservés.</p>
+    </footer>
+</body>
+</html>
