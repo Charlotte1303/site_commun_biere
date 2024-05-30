649c4577c580165765ff2d93c80681f321bdb9db
Reigneau <reigneau.a9@laposte.net>
2024-05-30 09:29:04 +0200

    encore

--- app/view/index.view.php
+++ app/view/index.view.php
@@ -1,64 +1,61 @@
-
 <body>
-    <main class="logo2">
-        
-        <img src="public/images/Logo.png" >
+  <main class="logo2">
+
+    <img src="public/images/Logo.png">
 
 
-    </main>
+  </main>
 
 
-    <div class="card-container">
-        <div class="card">
-            <div class="card-front">
-                <img src="public/images/biere.png" alt="Beer Image">
-            </div>
-            <div class="card-back">
-                <img src="public/images/story.png" alt="Story Image">
-            </div>
-        </div>
-        <div class="card">
-            <div class="card-front">
-                <img src="public/images/b2.png" alt="B2 Image">
-            </div>
-            <div class="card-back">
-                <img src="public/images/3.png" alt="3 Image">
-            </div>
-        </div>
-        <div class="card">
-            <div class="card-front">
-                <img src="public/images/b3.png" alt="B3 Image">
-            </div>
-            <div class="card-back">
-                <img src="public/images/5.png" alt="5 Image">
-            </div>
-        </div>
-        <div class="card">
-            <div class="card-front">
-                <img src="public/images/b4.png" alt="B4 Image">
-            </div>
-            <div class="card-back">
-                <img src="public/images/7.png" alt="7 Image">
-            </div>
-        </div>
-        <div class="card">
-            <div class="card-front">
-                <img src="public/images/b5.png" alt="B5 Image">
-            </div>
-            <div class="card-back">
-                <img src="public/images/9.png" alt="9 Image">
-            </div>
-        </div>
-        <div class="card">
-            <div class="card-front">
-                <img src="public/images/b6.png" alt="B6 Image">
-            </div>
-            <div class="card-back">
-                <img src="public/images/1.png" alt="11 Image">
-            </div>
-        </div>
+  <div class="card-container">
+    <div class="card">
+      <div class="card-front">
+        <img src="public/images/biere.png" alt="Beer Image">
+      </div>
+      <div class="card-back">
+        <img src="public/images/story.png" alt="Story Image">
+      </div>
     </div>
+    <div class="card">
+      <div class="card-front">
+        <img src="public/images/b2.png" alt="B2 Image">
+      </div>
+      <div class="card-back">
+        <img src="public/images/3.png" alt="3 Image">
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-front">
+        <img src="public/images/b3.png" alt="B3 Image">
+      </div>
+      <div class="card-back">
+        <img src="public/images/5.png" alt="5 Image">
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-front">
+        <img src="public/images/b4.png" alt="B4 Image">
+      </div>
+      <div class="card-back">
+        <img src="public/images/7.png" alt="7 Image">
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-front">
+        <img src="public/images/b5.png" alt="B5 Image">
+      </div>
+      <div class="card-back">
+        <img src="public/images/9.png" alt="9 Image">
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-front">
+        <img src="public/images/b6.png" alt="B6 Image">
+      </div>
+      <div class="card-back">
+        <img src="public/images/1.png" alt="11 Image">
+      </div>
+    </div>
+  </div>
 
 </body>
-
-
