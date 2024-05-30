d7bbfde307a3ca1eef5e7b1596a5f457307a1bd7
Reigneau <reigneau.a9@laposte.net>
2024-05-30 09:11:45 +0200

    cartes

--- app/view/index.view.php
+++ app/view/index.view.php
@@ -11,50 +11,50 @@
     <div class="card-container">
         <div class="card">
             <div class="card-front">
-                <img src="biere.png" alt="Beer Image">
+                <img src="public/images/biere.png" alt="Beer Image">
             </div>
             <div class="card-back">
-                <img src="story.png" alt="Story Image">
+                <img src="public/images/story.png" alt="Story Image">
             </div>
         </div>
         <div class="card">
             <div class="card-front">
-                <img src="b2.png" alt="B2 Image">
+                <img src="public/images/b2.png" alt="B2 Image">
             </div>
             <div class="card-back">
-                <img src="3.png" alt="3 Image">
+                <img src="public/images/3.png" alt="3 Image">
             </div>
         </div>
         <div class="card">
             <div class="card-front">
-                <img src="b3.png" alt="B3 Image">
+                <img src="public/images/b3.png" alt="B3 Image">
             </div>
             <div class="card-back">
-                <img src="5.png" alt="5 Image">
+                <img src="public/images/5.png" alt="5 Image">
             </div>
         </div>
         <div class="card">
             <div class="card-front">
-                <img src="b4.png" alt="B4 Image">
+                <img src="public/images/b4.png" alt="B4 Image">
             </div>
             <div class="card-back">
-                <img src="7.png" alt="7 Image">
+                <img src="public/images/7.png" alt="7 Image">
             </div>
         </div>
         <div class="card">
             <div class="card-front">
-                <img src="b5.png" alt="B5 Image">
+                <img src="public/images/b5.png" alt="B5 Image">
             </div>
             <div class="card-back">
-                <img src="9.png" alt="9 Image">
+                <img src="public/images/9.png" alt="9 Image">
             </div>
         </div>
         <div class="card">
             <div class="card-front">
-                <img src="b6.png" alt="B6 Image">
+                <img src="public/images/b6.png" alt="B6 Image">
             </div>
             <div class="card-back">
-                <img src="11.png" alt="11 Image">
+                <img src="1public/images/1.png" alt="11 Image">
             </div>
         </div>
     </div>
