c973b96ba446e391c2f843c3fc7724c65d4fa5db
Reigneau <reigneau.a9@laposte.net>
2024-05-30 09:08:44 +0200

    cartes_tournantes

--- app/view/index.view.php
+++ app/view/index.view.php
@@ -8,48 +8,56 @@
     </main>
 
 
-<div class="livres">
-  <div class="livre">
-    <img src="public/images/1.jpg" alt="Livre 1">
-    <div class="histoire">
-      <p>Notre bière blonde au Poivre de Sichuan, est une création inspirée de la tradition belge. Non filtrée et non pasteurisée, elle incarne la résilience du phénix avec sa saveur épicée et son caractère unique. 
-        C’est ne expérience gustative enflammée et renouvelée à chaque gorgée.</p>
-        <img src="public\images\smartmockups_lt8udws5.png">
+    <div class="card-container">
+        <div class="card">
+            <div class="card-front">
+                <img src="biere.png" alt="Beer Image">
+            </div>
+            <div class="card-back">
+                <img src="story.png" alt="Story Image">
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-front">
+                <img src="b2.png" alt="B2 Image">
+            </div>
+            <div class="card-back">
+                <img src="3.png" alt="3 Image">
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-front">
+                <img src="b3.png" alt="B3 Image">
+            </div>
+            <div class="card-back">
+                <img src="5.png" alt="5 Image">
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-front">
+                <img src="b4.png" alt="B4 Image">
+            </div>
+            <div class="card-back">
+                <img src="7.png" alt="7 Image">
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-front">
+                <img src="b5.png" alt="B5 Image">
+            </div>
+            <div class="card-back">
+                <img src="9.png" alt="9 Image">
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-front">
+                <img src="b6.png" alt="B6 Image">
+            </div>
+            <div class="card-back">
+                <img src="11.png" alt="11 Image">
+            </div>
+        </div>
     </div>
-  </div>
-  <div class="livre">
-    <img src="public/images/2.jpg" alt="Livre 2">
-    <div class="histoire">
-      <p>Notre Bière Ambrée au miel d'inspiration belge, non filtrée et non pasteurisée, symbolisée par le python. C’est un mélange parfait et   exquis de douceur du miel et de richesse maltée, offrant une expérience gustative unique et raffinée.</p>
-    </div>
-  </div>
-  <div class="livre">
-    <img src="public/images/3.jpg" alt="Livre 3">
-    <div class="histoire">
-      <p>Notre bière blanche fraîche au Pavot Bleu, inspirée de la tradition belge, est non filtrée et non pasteurisée pour une expérience authentique. Cette bière est symbolisée par un dragon serpent, incarnant la puissance. Son caractère rafraîchissant et son arôme unique, évoquent les mystères des contrées lointaines.</p>
-    </div>
-  </div>
-  <div class="livre">
-    <img src="public/images/4.jpg" alt="Livre 4">
-    <div class="histoire">
-      <p>Notre Bière Brune à la Lavande, d'inspiration belge, non filtrée et non pasteurisée, symbolisée par un ours blaireau. Cette bière associe habilement les saveurs riches et maltées d'une brune traditionnelle avec les notes délicates et florales de la lavande. 
-        Son caractère unique et son équilibre subtil entre douceur et amertume, créent une expérience de dégustation incomparable.</p>
-    </div>
-  </div>
-  <div class="livre">
-    <img src="public/images/5.jpg" alt="Livre 5">
-    <div class="histoire">
-      <p>Notre bière blonde à la menthe, est une création d'inspiration belge, non filtrée et non pasteurisée. Rafraîchissante et délicate, elle marie parfaitement les arômes frais de la menthe avec la richesse maltée de la bière blonde. Symbole d'élégance et de liberté, notre bière est représentée par un majestueux pégase, évoquant la légèreté et la puissance des saveurs qui dansent dans chaque gorgée.</p>
-    </div>
-  </div>
-  <div class="livre">
-    <img src="public/images/6.jpg" alt="Livre 6">
-    <div class="histoire">
-      <p>Notre Bière Blonde au curry éveillera vos papilles avec ses arômes exotiques et son caractère unique. Symbolisée par le majestueux pégase, cette bière incarne la fusion audacieuse entre la tradition brassicole belge et les épices exotiques du curry. 
-        Son équilibre subtil entre la douceur maltée et les notes épicées, emporte vers une expérience de dégustation inoubliable.</p>
-    </div>
-  </div>
-</div>
 
 </body>
 
