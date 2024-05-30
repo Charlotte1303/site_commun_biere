3dd78e6941ffa68126a08f33a0dcb3dc4983691f
Charlotte1303 <144892844+Charlotte1303@users.noreply.github.com>
2024-05-30 23:36:21 +0200

    brassage

--- app/view/brassage.view.php
+++ app/view/brassage.view.php
@@ -1,10 +1,36 @@
 <body>
-<main>
-    <div class="titre">
-        <h1>LE BRASSAGE DE NOS BIERES</h1>
-    </div>
-    <section class="poster">
-        <img src="public\images\MMI_G-1_Poster_Festival_Enzyme.png" alt="poster des étapes du brassage de la bière">
-    </section>
-</main>
+    <main>
+        <div class="titre">
+            <h1>LE BRASSAGE DE NOS BIERES</h1>
+        </div>
+        <section class="poster">
+            <img src="public\images\MMI_G-1_Poster_Festival_Enzyme.png" alt="poster des étapes du brassage de la bière">
+        </section>
+        <section class="etape">
+            <div class="contenu">
+                <img src="public\images\mockupjaune.png" alt="Description de l'étape">
+                <div class="texte">
+                    <h2>1.</h2>
+                    <p>Nous sommes 4 étudiants en Génie Biologique passionnés de sciences. Notre projet est de créer une bière en collaboration avec 5 étudiants en formation des métiers du multimédia et de l'internet. C’est ainsi qu’est née la marque de bière Infinité.</p>
+                </div>
+            </div>
+        </section>
+        <section class="etape">
+            <div class="contenu">
+                <img src="public\images\mockupbleu.png" alt="Image étape 2">
+                <div class="texte">
+                    <h2>2.</h2>
+                    <p>Ingrédients :
+                        Orge malté (Malt Pale Ale 75%, Vienna 25%) 7,67%
+                        Houblon Nelson Sauvin 0,17%
+                        Eau 92,04%
+                        Levures Nottingham 0,04%
+                        Graines de coriandre 0,05%
+                        Poivre de Sichuan 0,03%
+                    </p>
+                </div>
+            </div>
+        </section>
+
+    </main>
 </body>
