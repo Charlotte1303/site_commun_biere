6a1c1e18aa4945a171c8ca015d71ade20dca5772
Charlotte1303 <144892844+Charlotte1303@users.noreply.github.com>
2024-05-30 09:58:10 +0200

    bh

--- app/view/brassage.view.php
+++ app/view/brassage.view.php
@@ -1,4 +1,4 @@
-
+<body>
 <main>
     <div class="titre">
         <h1>LE BRASSAGE DE NOS BIERES</h1>
@@ -7,4 +7,4 @@
         <img src="public\images\MMI_G-1_Poster_Festival_Enzyme.png" alt="poster des étapes du brassage de la bière">
     </section>
 </main>
-
+</body>
