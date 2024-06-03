1d317f5ce74a7c28d6da8dd27dd86d8a27fd0c17
Ahinoa Ruiz <ahinoaruizramirez@gmail.com>
2024-06-03 08:41:45 +0200

    CHGMT BIERES

--- app/view/biere.view.php
+++ app/view/biere.view.php
@@ -1,28 +1,22 @@
-<?php
-session_start();
-if (!isset($_SESSION['age_verified']) || $_SESSION['age_verified'] !== true) {
-    header('Location: age_verification.php');
-    exit;
-}
-?>
-
 <main> 
 <div class="beer-description">
-  <h2>SOUFFLE SICHUANAIS</h2>
+<?php foreach ($bieres as $aBiere) : ?>
+  <h2><?= $aBiere['Nom'] ?></h2>
   <p>Une bière qui vous fera voyager à travers un mélange d'agrumes et épices, avec le poivre de Sichuan.</p>
-  <p>Bière blonde au poivre de Sichuan d'inspiration belge non filtrée non pasteurisée.</p>
+  <p><?= $aBiere['Description'] ?></p>
   <h3>Ingrédients:</h3>
   <ul>
-    <li>Eau: 92,04%</li>
-    <li>Malts</li>
-    <li>Houblon: 0,17%</li>
-    <li>Levure: 0,04%</li>
-    <li>Poivre de Sichuan: 0,03%</li>
-    <li>Graines de coriandre: 0,05%</li>
+  <?php
+    $ingredients = explode('/', $aBiere['Ingredients']);
+    foreach ($ingredients as $ingredient) {
+        echo '<li>'. trim($ingredient). '</li>';
+    }
+   ?>
   </ul>
   <p>Amertume: <span>(no value provided)</span></p>
   <p>Coloration: <span>(no value provided)</span></p>
-  <p>Contient du gluten</p>
+  <p><?= $aBiere['Valeurs_nutritionnelles'] ?></p>
 </div>
-<img src="public/images/smartmockups_lt8udws5.png" alt="Beer Portrait" class="portrait-image">
+<img src="public/images/<?= $aBiere['Photo'] ?>" alt="Beer Portrait" class="portrait-image">
+<?php endforeach ?>
 </main>
