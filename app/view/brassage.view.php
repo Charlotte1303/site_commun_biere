6c3dab9d5e66fa6709ccff77ed54958c24b70267
Charlotte1303 <144892844+Charlotte1303@users.noreply.github.com>
2024-06-03 10:28:59 +0200

    nl

--- app/view/brassage.view.php
+++ app/view/brassage.view.php
@@ -8,7 +8,7 @@
         </section>
         <section class="etape">
             <div class="contenu">
-                <img src="public\images\mockupjaune.png" alt="Description de l'étape">
+                <img src="public\images\etape1.png" alt="Image de l'étape 1">
                 <div class="texte">
                     <h2>1.</h2>
                     <p>Nous sommes 4 étudiants en Génie Biologique passionnés de sciences. Notre projet est de créer une bière en collaboration avec 5 étudiants en formation des métiers du multimédia et de l'internet. C’est ainsi qu’est née la marque de bière Infinité.</p>
@@ -17,7 +17,7 @@
         </section>
         <section class="etape">
             <div class="contenu">
-                <img src="public\images\mockupbleu.png" alt="Image étape 2">
+                <img src="public\images\etape2.jpg" alt="Image étape 2">
                 <div class="texte">
                     <h2>2.</h2>
                     <p>Ingrédients :
@@ -31,6 +31,129 @@
                 </div>
             </div>
         </section>
+        <section class="etape">
+            <div class="contenu">
+                <img src="public\images\etape3.jpg" alt="Image étape 3">
+                <div class="texte">
+                    <h2>3.</h2>
+                    <p>But : Ouvrir les grains pour libérer l'amidon et l'exposer aux enzymes, des “ciseaux moléculaires“.
+                        Moyen : Concassage des grains de malt d'orge avec un moulin à malt, ce qui permet l'exposition de l'amidon.
 
+                    </p>
+                </div>
+            </div>
+        </section>
+        <section class="etape">
+            <div class="contenu">
+                <img src="public\images\etape4.jpg" alt="Image étape 4">
+                <div class="texte">
+                    <h2>4.</h2>
+                    <p>But : Extraire le sucre des grains concassés grâce aux enzymes qui découpent l'amidon en glucose.
+                        Moyen : Verser les grains concassés dans 22 L d'eau chauffée à 72°C afin d'obtenir un mélange appelé “moût“.
+
+                    </p>
+                </div>
+            </div>
+        </section>
+        <section class="etape">
+            <div class="contenu">
+                <img src="public\images\etape5.jpg" alt="Image étape 5">
+                <div class="texte">
+                    <h2>5.</h2>
+                    <p>But : Stériliser et ajouter les houblons à la bière pour apporter des saveurs.
+                        Moyen : Les houblons sont ajoutés au moût en début d'ébullition afin d'apporter de l'amertume, et à la fin pour apporter des arômes. Le tout est chauffé à 100°C pour stériliser la bière.
+
+                    </p>
+                </div>
+            </div>
+        </section>
+        <section class="etape">
+            <div class="contenu">
+                <img src="public\images\etape6.jpg" alt="Image étape 6">
+                <div class="texte">
+                    <h2>6.</h2>
+                    <p>But : Transformer le glucose en éthanol. Cette étape est effectuée deux fois : en cuve puis en bouteille.
+                        Moyen : Les levures Nottingham nous permettent d'obtenir une bière blonde au goût naturel avec quelques notes d'agrumes.
+
+                    </p>
+                </div>
+            </div>
+        </section>
+        <section class="etape">
+            <div class="contenu">
+                <img src="public\images\etape7.jpg" alt="Image étape 7">
+                <div class="texte">
+                    <h2>7.</h2>
+                    <p>But : Déterminer la fin de la fermentation et éventuellement détecter une contamination.
+                        Résultats :
+                        La densité se stabilise à 1,005 au bout de 20 jours.
+                        Le pH se stabilise à 4,39 (acide) au bout de 2 mois et demi.
+
+                    </p>
+                </div>
+            </div>
+        </section>
+        <section class="etape">
+            <div class="contenu">
+                <img src="public\images\etape8.jpg" alt="Image étape 8">
+                <div class="texte">
+                    <h2>8.</h2>
+                    <p>But : Obtenir un faible taux d’alcool pour que la bière soit accessible à tous et agréable en été.
+                        Résultats : On obtient un taux d’alcool de 2,8% vol, donc une bière faiblement alcoolisée.
+
+                    </p>
+                </div>
+            </div>
+        </section>
+        <section class="etape">
+            <div class="contenu">
+                <img src="public\images\etape9.jpg" alt="Image étape 9">
+                <div class="texte">
+                    <h2>9.</h2>
+                    <p>But : Déterminer la couleur de la bière en calculant un indice, l'EBC, à partir de la capacité des levures à atténuer la lumière.
+                        Résultats : On obtient un EBC de 20,5 donc il s'agit bien d'une bière dorée.
+
+                    </p>
+                </div>
+            </div>
+        </section>
+        <section class="etape">
+            <div class="contenu">
+                <img src="public\images\etape10.jpg" alt="Image étape 10">
+                <div class="texte">
+                    <h2>10.</h2>
+                    <p>But : S'assurer qu'il n'y ait aucune contamination microbienne.
+                        Résultats : Seuil de toxicité non franchi, la bière est consommable sans risque pour la santé.
+
+                    </p>
+                </div>
+            </div>
+        </section>
+        <section class="etape">
+            <div class="contenu">
+                <img src="public\images\etape11.jpg" alt="Image étape 11">
+                <div class="texte">
+                    <h2>11.</h2>
+                    <p>But : Obtenir un aspect visuel et organoleptique agréable qui renferme des saveurs.
+                        Résultats : La probabilité qu'une bulle éclate est très faible (0,009), donc la mousse est stable.
+
+                    </p>
+                </div>
+            </div>
+        </section>
+        <section class="etape">
+            <div class="contenu">
+                <img src="public\images\Logo.png" alt="Image étape 12">
+                <div class="texte">
+                    <h2>12.</h2>
+                    <p>En verre, les bouteilles de bière ont un impact moins significatif sur la planète que les bières en fûts et en canettes. Cependant, il est important de bien les recycler. En France, 1 bouteille sur 3 n'est pas recyclée d'après Zero Waste. La bière pollue aussi tout au long de sa vie, dès la production des matières premières, jusqu'au consommateur.
+
+                    </p>
+                </div>
+            </div>
+        </section>
+        <div class="titre">
+            <h1>Choisir une bière <font color="#EDE486">artisanale locale</font>, c'est réduire son impact <font color="#ABED86">environnemental</font> !</h1>
+        </div>
     </main>
 </body>
