95523e4df6ddfb52a28691a8901331491e97e503
Charlotte1303 <144892844+Charlotte1303@users.noreply.github.com>
2024-06-03 09:19:53 +0200

    ajout lien du jeu

--- app/view/a_propos.view.php
+++ app/view/a_propos.view.php
@@ -1,87 +1,70 @@
+<style>
+    /* Importer la police depuis une source externe ou localement */
+    @font-face {
+        font-family: "BodoniFLF-Bold";
+        src: url(font/BodoniFLF-Bold.ttf);
+    }
 
-    <style>
-        /* Importer la police depuis une source externe ou localement */
-        @font-face {
-            font-family: "BodoniFLF-Bold";
-            src: url(font/BodoniFLF-Bold.ttf);
-        }
+    /* Appliquer la police à la balise h1 */
+    h1 {
+        font-family: "BodoniFLF-Bold", sans-serif;
+        /* Utiliser BodoniFLF-Bold, sinon une police de secours sans empattement */
+    }
 
-        /* Appliquer la police à la balise h1 */
-        h1 {
-            font-family: "BodoniFLF-Bold", sans-serif;
-            /* Utiliser BodoniFLF-Bold, sinon une police de secours sans empattement */
-        }
+    /* Importer la police Robots-Black depuis une source externe ou localement */
+    @font-face {
+        font-family: "Robots-Black";
+        src: url(font/Robots-Black.ttf);
+    }
 
-        /* Importer la police Robots-Black depuis une source externe ou localement */
-        @font-face {
-            font-family: "Robots-Black";
-            src: url(font/Robots-Black.ttf);
-        }
+    /* Appliquer la police Robots-Black à la balise h2 */
+    h2 {
+        font-family: "Robots-Black", sans-serif;
+        /* Utiliser Robots-Black, sinon une police de secours sans empattement */
+    }
 
-        /* Appliquer la police Robots-Black à la balise h2 */
-        h2 {
-            font-family: "Robots-Black", sans-serif;
-            /* Utiliser Robots-Black, sinon une police de secours sans empattement */
-        }
+    /* Importer la police Roboto-Light depuis une source externe ou localement */
+    @font-face {
+        font-family: "Roboto-Light";
+        src: url(font/Roboto-Light.ttf);
+    }
 
-        /* Importer la police Roboto-Light depuis une source externe ou localement */
-        @font-face {
-            font-family: "Roboto-Light";
-            src: url(font/Roboto-Light.ttf);
-        }
-
-        /* Appliquer la police Roboto-Light à la balise h3 */
-        h3 {
-            font-family: "Roboto-Light", sans-serif;
-            /* Utiliser Roboto-Light, sinon une police de secours sans empattement */
-        }
-    </style>
+    /* Appliquer la police Roboto-Light à la balise h3 */
+    h3 {
+        font-family: "Roboto-Light", sans-serif;
+        /* Utiliser Roboto-Light, sinon une police de secours sans empattement */
+    }
+</style>
 </head>
 
 <main>
-    <section class="container">
-        <h1>INFINITÉ</h1>
-        <h2>À PROPOS</h2>
-        <h3>Infinité, c'est l'histoire de neuf jeunes passionnés par la bière et l'univers fantastique.
-            <br>Nées d'une envie de créer des saveurs
-            uniques et ensorcelantes, nos bières vous invitent à <br>un
-            voyage gustatif et imaginaire sans limite.
-        </h3>
-        <img src="public/images/étoiles.png" alt="étoiles">
-        <h2 class="nouveau-titre">QUI SOMMES-NOUS ?</h2>
-        <div class="photos">
-            <a href="membres.php"> <img class="photo_profil" src="public/images/photo_profil.png" alt="Photo profil 1"></a>
-            <a href="index.html"> <img class="photo_profil" src="public/images/photo_profil.png" alt="Photo profil 2"></a>
-            <a href="index.html"><img class="photo_profil" src="public/images/photo_profil.png" alt="Photo profil 3"></a>
-            <a href="index.html"><img class="photo_profil" src="public/images/photo_profil.png" alt="Photo profil 4"></a>
-            <a href="index.html"><img class="photo_profil" src="public/images/photo_profil.png" alt="Photo profil 5"></a>
-        </div>
-        <div class="nouvelle_photo">
-            <a href="index.html"><img class="photo_profil" src="public/images/photo_profil.png" alt="Photo profil 6"></a>
-            <a href="index.html"><img class="nouvelle_photo" src="public/images/photo_profil.png" alt="Photo profil 7"></a>
-            <a href="index.html"><img class="nouvelle_photo" src="public/images/photo_profil.png" alt="Photo profil 8"></a>
-            <a href="index.html"><img class="nouvelle_photo" src="public/images/photo_profil.png" alt="Photo profil 9"></a>
+        <section class="container">
+            <h1>INFINITÉ</h1>
+            <h2>À PROPOS</h2>
+            <h3>Infinité, c'est l'histoire de neuf jeunes passionnés par la bière et l'univers fantastique.
+                <br>Nées d'une envie de créer des saveurs
+                uniques et ensorcelantes, nos bières vous invitent à <br>un
+                voyage gustatif et imaginaire sans limite.
+            </h3>
+            <img src="public/images/étoiles.png" alt="étoiles">
+            <h2 class="nouveau-titre">QUI SOMMES-NOUS ?</h2>
+            <div class="photos">
+                <a href="membres.php"> <img class="photo_profil" src="public/images/photo_profil.png" alt="Photo profil 1"></a>
+                <a href="index.html"> <img class="photo_profil" src="public/images/photo_profil.png" alt="Photo profil 2"></a>
+                <a href="index.html"><img class="photo_profil" src="public/images/photo_profil.png" alt="Photo profil 3"></a>
+                <a href="index.html"><img class="photo_profil" src="public/images/photo_profil.png" alt="Photo profil 4"></a>
+                <a href="index.html"><img class="photo_profil" src="public/images/photo_profil.png" alt="Photo profil 5"></a>
+            </div>
+            <div class="nouvelle_photo">
+                <a href="index.html"><img class="photo_profil" src="public/images/photo_profil.png" alt="Photo profil 6"></a>
+                <a href="index.html"><img class="nouvelle_photo" src="public/images/photo_profil.png" alt="Photo profil 7"></a>
+                <a href="index.html"><img class="nouvelle_photo" src="public/images/photo_profil.png" alt="Photo profil 8"></a>
+                <a href="index.html"><img class="nouvelle_photo" src="public/images/photo_profil.png" alt="Photo profil 9"></a>
+            </div>
+            <div class="jeu">
+                <h2 class="nouveau-titre">NOTRE JEU</h2>
+                <a class="lien" href="https://infiinite.alwaysdata.net/"><strong>Infinité le jeu !</strong></a>
+            </div>
             <video class="video_backstage" src="video_backstage.mp4" controls></video>
-        </div>
-    </section>
-</main>
-<div class="paillette" style="left: 10%; animation-delay: 0.5s;"></div>
-<div class="paillette" style="left: 20%; animation-delay: 1s;"></div>
-<div class="paillette" style="right: 5%; animation-delay: 1.5s;"></div>
-<!-- Ajoutez plus de paillettes -->
-<div class="paillette" style="left: 30%; animation-delay: 2s;"></div>
-<div class="paillette" style="right: 15%; animation-delay: 2.5s;"></div>
-<div class="paillette" style="left: 40%; animation-delay: 3s;"></div>
-<div class="paillette" style="right: 20%; animation-delay: 3.5s;"></div>
-<div class="paillette" style="left: 50%; animation-delay: 4s;"></div>
-<div class="paillette" style="right: 25%; animation-delay: 4.5s;"></div>
-<div class="paillette" style="left: 10%; animation-delay: 0.5s;"></div>
-<div class="paillette" style="left: 20%; animation-delay: 1s;"></div>
-<div class="paillette" style="right: 5%; animation-delay: 1.5s;"></div>
-<!-- Ajoutez plus de paillettes -->
-<div class="paillette" style="left: 30%; animation-delay: 2s;"></div>
-<div class="paillette" style="right: 15%; animation-delay: 2.5s;"></div>
-<div class="paillette" style="left: 40%; animation-delay: 3s;"></div>
-<div class="paillette" style="right: 20%; animation-delay: 3.5s;"></div>
-<div class="paillette" style="left: 50%; animation-delay: 4s;"></div>
-<div class="paillette" style="right: 25%; animation-delay: 4.5s;"></div>
+        </section>
+    </main>
