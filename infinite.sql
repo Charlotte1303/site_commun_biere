008e20e5e16f77d2c11ab0ec67f16601c15b3291
Ahinoa Ruiz <ahinoaruizramirez@gmail.com>
2024-06-03 12:21:39 +0200

    update sql, à changer dans sa bdd

--- infinite.sql
+++ infinite.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 30 mai 2024 à 21:06
+-- Généré le : lun. 03 juin 2024 à 10:21
 -- Version du serveur : 8.0.31
--- Version de PHP : 8.0.26
+-- Version de PHP : 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,11 +29,11 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `avis`;
 CREATE TABLE IF NOT EXISTS `avis` (
-  `idAvis` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `Texte` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  `idAvis` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `Texte` text COLLATE utf8mb4_general_ci,
   `Note` decimal(2,1) NOT NULL,
   `Date_de_publication` date NOT NULL,
-  `idClient` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `idClient` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`idAvis`),
   KEY `idClient` (`idClient`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -46,16 +46,18 @@ CREATE TABLE IF NOT EXISTS `avis` (
 
 DROP TABLE IF EXISTS `biere`;
 CREATE TABLE IF NOT EXISTS `biere` (
-  `idBiere` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `Nom` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `idBiere` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `Nom` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   `Degre_d_alcool` decimal(3,1) NOT NULL,
   `Ingredients` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `Contenance` int NOT NULL,
   `Prix` decimal(4,2) NOT NULL,
   `Description` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `Valeurs_nutritionnelles` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `Disponibilite` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `Photo` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `Disponibilite` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `Photo` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `Bouteille` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `Accroche` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`idBiere`),
   UNIQUE KEY `Nom` (`Nom`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -64,13 +66,13 @@ CREATE TABLE IF NOT EXISTS `biere` (
 -- Déchargement des données de la table `biere`
 --
 
-INSERT INTO `biere` (`idBiere`, `Nom`, `Degre_d_alcool`, `Ingredients`, `Contenance`, `Prix`, `Description`, `Valeurs_nutritionnelles`, `Disponibilite`, `Photo`) VALUES
-('1', 'Souffle Sichuanais', '5.5', 'Ingrédients: eau 92,04%, malts, houblon 0,17%, levure 0,04%, poivre de Sichuan 0,03%, graines de coriandre 0,05%.', 75, '3.00', 'Bière blonde au poivre de Sichuan d’inspiration belge non filtrée non pasteurisée.', 'Contient du gluten', 'oui', 'mockuprougeredimensionne.png'),
-('2', 'Venin sucré', '7.0', 'Ingrédients: eau 92,04%, malts, houblon 0,17%, levure 0,04%, poivre de miel 0,03%, graines de coriandre 0,05%.', 75, '3.00', 'Bière ambrée miel d’inspiration belge non filtrée non pasteurisée. ', 'Contient du gluten', 'oui', 'mockuporangeredimensionne.png'),
-('3', 'Pavot Abyssal', '6.0', 'Ingrédients: eau 92,04%, malts, houblon 0,17%, levure 0,04%, fleur de pavot bleu 0,03%, graines de coriandre 0,05%.', 75, '3.00', 'Bière blanche au pavot bleu d’inspiration belge non filtrée non pasteurisée.', 'Contient du gluten', 'Oui', 'mockupbleuredimensionne.png'),
-('4', 'Lavande Cosmique', '5.0', 'Ingrédients: eau 92,04%, malts, houblon 0,17%, levure 0,04%, lavande 0,03%, graines de coriandre 0,05%.', 75, '3.00', 'Bière brune à la lavande d’inspiration belge non filtrée non pasteurisée.', 'Contient du gluten', 'Oui', 'mockupvioletredimensionne.png'),
-('5', 'Menthe céleste', '6.0', 'Ingrédients: eau 92,04%, malts, houblon 0,17%, levure 0,04%, menthe 0,03%, graines de coriandre 0,05%.', 75, '3.00', 'Bière ambrée mentholée d’inspiration belge non filtrée non pasteurisée.', 'Contient du gluten', 'Oui', 'mockupvertredimensionne.png'),
-('6', 'Soleil Ardent', '7.0', 'Ingrédients: eau 92,04%, malts, houblon 0,17%, lev', 75, '3.00', 'Bière blonde au curry d’inspiration belge non filtrée non pasteurisée.', 'Contient du gluten', 'Oui', 'mockupjauneredimensionne.png');
+INSERT INTO `biere` (`idBiere`, `Nom`, `Degre_d_alcool`, `Ingredients`, `Contenance`, `Prix`, `Description`, `Valeurs_nutritionnelles`, `Disponibilite`, `Photo`, `Bouteille`, `Accroche`) VALUES
+('1', 'Souffle Sichuanais', '5.5', 'eau : 92,04%/malts/houblon : 0,17%/levure : 0,04%/poivre de Sichuan: 0,03%/graines de coriandre : 0,05%', 75, '3.00', 'Bière blonde au poivre de Sichuan d’inspiration belge non filtrée non pasteurisée.', 'Contient du gluten', 'oui', 'mockuprougeredimensionne.png', 'bouteillerouge.png', 'Une bière qui vous fera voyager à travers un mélange d\'agrumes et épices, avec le poivre de Sichuan.'),
+('2', 'Venin sucré', '7.0', 'eau : 92,04%/malts/houblon : 0,17%/levure : 0,04%/miel : 0,03%/graines de coriandre : 0,05%', 75, '3.00', 'Bière ambrée miel d’inspiration belge non filtrée non pasteurisée. ', 'Contient du gluten', 'oui', 'mockuporangeredimensionne.png', 'bouteilleorange.png', 'Une bière qui vous fera voyager à travers un mélange d\'agrumes et épices, avec du miel.'),
+('3', 'Pavot Abyssal', '6.0', 'eau : 92,04%/malts/houblon : 0,17%/levure : 0,04%/fleur de pavot : 0,03%/graines de coriandre : 0,05%', 75, '3.00', 'Bière blanche au pavot bleu d’inspiration belge non filtrée non pasteurisée.', 'Contient du gluten', 'Oui', 'mockupbleuredimensionne.png', 'bouteillebleue.png', 'Une bière qui vous fera voyager à travers un mélange d\'agrumes et épices, avec de la fleur de pavot.'),
+('4', 'Lavande Cosmique', '5.0', 'eau : 92,04%/malts/houblon : 0,17%/levure : 0,04%/lavande : 0,03%/graines de coriandre : 0,05%', 75, '3.00', 'Bière brune à la lavande d’inspiration belge non filtrée non pasteurisée.', 'Contient du gluten', 'Oui', 'mockupvioletredimensionne.png', 'bouteilleviolette.png', 'Une bière qui vous fera voyager à travers un mélange d\'agrumes et épices, avec de la lavande.'),
+('5', 'Menthe céleste', '6.0', 'eau : 92,04%/malts/houblon : 0,17%/levure : 0,04%/menthe : 0,03%/graines de coriandre : 0,05%', 75, '3.00', 'Bière ambrée mentholée d’inspiration belge non filtrée non pasteurisée.', 'Contient du gluten', 'Oui', 'mockupvertredimensionne.png', 'bouteilleverte.png', 'Une bière qui vous fera voyager à travers un mélange d\'agrumes et épices, avec de la menthe.'),
+('6', 'Soleil Ardent', '7.0', 'eau : 92,04%/malts/houblon : 0,17%/levure : 0,04%/curry : 0,03%/graines de coriandre : 0,05%', 75, '3.00', 'Bière blonde au curry d’inspiration belge non filtrée non pasteurisée.', 'Contient du gluten', 'Oui', 'mockupjauneredimensionne.png', 'bouteillejaune.png', 'Une bière qui vous fera voyager à travers un mélange d\'agrumes et épices, avec du curry.');
 
 -- --------------------------------------------------------
 
@@ -80,13 +82,13 @@ INSERT INTO `biere` (`idBiere`, `Nom`, `Degre_d_alcool`, `Ingredients`, `Contena
 
 DROP TABLE IF EXISTS `client`;
 CREATE TABLE IF NOT EXISTS `client` (
-  `idClient` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `Prénom` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `Nom` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `Adresse_email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `Telephone` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `idClient` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `Prénom` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `Nom` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `Adresse_email` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `Telephone` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   `Date_de_naissance` int NOT NULL,
-  `Adresse` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `Adresse` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`idClient`),
   UNIQUE KEY `Adresse_email` (`Adresse_email`),
   UNIQUE KEY `Telephone` (`Telephone`)
@@ -108,10 +110,10 @@ INSERT INTO `client` (`idClient`, `Prénom`, `Nom`, `Adresse_email`, `Telephone`
 
 DROP TABLE IF EXISTS `commande`;
 CREATE TABLE IF NOT EXISTS `commande` (
-  `idCommande` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `Statut` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `idCommande` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `Statut` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   `Date_de_commande` date NOT NULL,
-  `idClient` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `idClient` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`idCommande`),
   KEY `idClient` (`idClient`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -132,8 +134,8 @@ INSERT INTO `commande` (`idCommande`, `Statut`, `Date_de_commande`, `idClient`) 
 
 DROP TABLE IF EXISTS `contenir`;
 CREATE TABLE IF NOT EXISTS `contenir` (
-  `idBiere` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `idCommande` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `idBiere` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `idCommande` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   `quantité` int NOT NULL,
   PRIMARY KEY (`idBiere`,`idCommande`),
   KEY `idCommande` (`idCommande`)
@@ -147,21 +149,13 @@ CREATE TABLE IF NOT EXISTS `contenir` (
 
 DROP TABLE IF EXISTS `formulaire_de_contact`;
 CREATE TABLE IF NOT EXISTS `formulaire_de_contact` (
-  `idFormulaire_de_contact` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `idFormulaire_de_contact` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   `message` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `email` varchar(90) COLLATE utf8mb4_general_ci NOT NULL,
-  `nom` varchar(90) COLLATE utf8mb4_general_ci NOT NULL,
-  `idClient` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `Date_message` date NOT NULL,
+  `idClient` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`idFormulaire_de_contact`),
   KEY `idClient` (`idClient`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `formulaire_de_contact`
---
-
-INSERT INTO `formulaire_de_contact` (`idFormulaire_de_contact`, `message`, `email`, `nom`, `idClient`) VALUES
-('', 'test\r\n', 'jeanvoinecharlotte@gmail.com', 'Charlotte Jeanvoine', ''),
-('', 'test2', 'jeanvoinecharlotte@gmail.com', 'Charlotte Jeanvoine', '');
 
 -- --------------------------------------------------------
 
@@ -171,10 +165,10 @@ INSERT INTO `formulaire_de_contact` (`idFormulaire_de_contact`, `message`, `emai
 
 DROP TABLE IF EXISTS `paiement`;
 CREATE TABLE IF NOT EXISTS `paiement` (
-  `idPaiement` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `idPaiement` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   `Montant` decimal(5,2) NOT NULL,
   `Date_de_paiement` date NOT NULL,
-  `idCommande` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `idCommande` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`idPaiement`),
   UNIQUE KEY `idCommande` (`idCommande`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
