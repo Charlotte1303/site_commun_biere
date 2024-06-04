1ceb2abebcbe08d7b030f774721177c1e5e7e8de
Ahinoa Ruiz <ahinoaruizramirez@gmail.com>
2024-06-04 13:05:15 +0200

    Changements header sur  toutes les pages css + animation nosbieres fonctionnelle

--- app/view/nos_bieres.view.php
+++ app/view/nos_bieres.view.php
@@ -1,10 +1,11 @@
 <main>
-<script src="public/js/app.js"></script>
+
     <!-- carousel -->
     <div class="carousel">
         <!-- list item -->
+
         <div class="list">
-            <?php foreach ($bieres as $aBiere) :?>
+        <?php foreach ($bieres as $aBiere) :?>
                 <div class="item">
                     <img src="public/images/<?= htmlspecialchars($aBiere['Photo'])?>">
                     <div class="content">
@@ -13,21 +14,33 @@
                         <div class="topic"><?= htmlspecialchars($aBiere['Description'])?></div>
                         <div class="buttons"><button><a href="biere.php?id=<?= $aBiere['idBiere'] ?>">ACHETER</a></button></div>
                     </div>
+                    
                 </div>
-            <?php endforeach?>
+                <?php endforeach?>
+        </div>
 
+                 
+
+        <div class="thumbnail">
+        <?php foreach ($bieres as $aBiere) :?>
+            <div class="item">
+                        <img src="public/images/<?= htmlspecialchars($aBiere['Photo'])?>">
+            </div>
+        <?php endforeach?>
+        </div>
+
+       
+            
             <!-- next prev -->
 
             <div class="arrows">
-                <button id="prev"><</button>
-                <button id="next">></button>
+                <button id="next"><img src="public/images/fleche_droite.png"/></button>
             </div>
 
             <!-- time running -->
             <div class="time"></div>
         </div>
 
-       
-        
     </div>
+    <script src="public/js/app.js"></script>
 </main>
--- public/js/script.js
+++ public/js/script.js
@@ -1,35 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let content = document.querySelector('.content');
-    let fleche = document.getElementById('fleche');
-
-    content.addEventListener('scroll', function() {
-        // Si la position de défilement verticale est supérieure à 100 pixels
-        if (document.body.scrollHeight > 10) {
-            // Ajouter la classe "invisible" pour cacher la flèche
-            fleche.classList.add('invisible');
-        } else {
-            // Sinon, enlever la classe "invisible" pour montrer la flèche
-            fleche.classList.remove('invisible');
-        }
-    });
-
-    // Get the beer list items
-    var beerListItems = document.querySelectorAll('.beer-list-item');
-
-    // Set the current index to 0
-    var currentIndex = 0;
-
-    // Set the timer for the carousel
-    setInterval(function() {
-        // Move to the next item
-        currentIndex++;
-
-        // If we reached the end of the list, wrap around to the beginning
-        if (currentIndex >= beerListItems.length) {
-            currentIndex = 0;
-        }
-
-        // Scroll to the current item
-        beerListItems[currentIndex].scrollIntoView();
-    }, 3000); // 3 seconds
+window.addEventListener('scroll', function() {
+    // Si la position de défilement verticale est supérieure à 100 pixels
+    if (window.scrollY > 100) {
+        // Ajouter la classe "invisible" pour cacher la flèche
+        fleche.classList.add('invisible');
+    } else {
+        // Sinon, enlever la classe "invisible" pour montrer la flèche
+        fleche.classList.remove('invisible');
+    }
 });
