3c1ed9f1d612a35a09c03c7871cdc67562b3277f
Reigneau <reigneau.a9@laposte.net>
2024-06-04 09:50:28 +0200

    ambreeeeee

--- database.php
+++ database.php
@@ -5,9 +5,9 @@ $trombi = [
         'photo' => 'public/images/Ambre.png',
         'prenom' => 'Ambre',
         'age' => '20',
-        'descriptif' => "Une rêveuse fan de cinéma et de graphisme. Ceux qui me connaissent disent de moi que je suis un esprit créatif, mais surtout rigoureux et perfectionniste ! 
-        En dehors de mes études, vous me trouverez souvent devant un bon film ou en train de travailler sur un projet de design graphique. 
-        Mon rêve est de combiner mes passions pour le cinéma et le graphisme pour créer des œuvres visuelles qui racontent des histoires captivantes et émouvantes.
+        'descriptif' => "Hello ! Moi c'est Ambre, une rêveuse fan de cinéma et de graphisme. 🌙 🎬 ✍️ <br> Ceux qui me connaissent disent de moi que je suis un esprit créatif, mais surtout rigoureux <br> et perfectionniste ! 
+        En dehors de mes études, vous me trouverez souvent devant un bon film ou <br> en train de travailler sur un projet de design graphique. 
+        Mon rêve est de combiner mes passions <br> pour le cinéma et le graphisme pour créer des œuvres visuelles qui racontent des histoires <br> captivantes et émouvantes.
         ",
     ],
     2 => [
@@ -15,56 +15,62 @@ $trombi = [
         'photo' => 'public/images/Charlotte.png',
         'prenom' => 'Charlotte',
         'age' => '19',
-        'descriptif' => "Étudiante de 1ere année de MMI, fan de livre, d’écriture et de dessin, et amatrice de cinéma.",
+        'descriptif' => "Salut ! C'est Charlotte, j'aime dessiner et écrire des histoires un peu cliché 😂 <br> Je suis étudiante à l'IUT de Sénart en formation des métiers du multimédia et <br> de l'internet (c'est plutôt cool).",
     ],
     3 => [
         'id' => 3,
         'photo' => 'public/images/Ahinoa.png',
         'prenom' => 'Ahinoa',
         'age' => '19',
-        'descriptif' => " ",
+        'descriptif' => "lalala ",
     ],
     4 => [
         'id' => 4,
         'photo' => 'public/images/Alexandre.png',
         'prenom' => 'Alexandre',
         'age' => '19',
-        'descriptif' => "1ere année de BUT MMI, je joue aux vidéos et je dessine beaucoup.",
+        'descriptif' => "Hey, moi c’est Alexandre ! J’ai 19 ans et je suis passionné de musique, j’en joue et j’en compose <br> seul ou avec des amis. 🥁 J’aime aussi l’audiovisuel et son côté artistique qui permet d’exprimer des émotions à l’image ou au son. 📸 Travailler sur la marque de bière Infinité m’a énormément plu, <br> l’univers que nous avons créé est aussi immersif que débordant d’imagination et j’espère <br> qu’il vous plaira autant qu’à nous ! ✨",
     ],
     5 => [
         'id' => 5,
         'photo' => 'public/images/Ando.png',
         'prenom' => 'Ando',
         'age' => '20',
-        'descriptif' => "Je m'appelle Andoniaina Tombozaza, mais mon surnom c'est Ando. En quelques mots : accros au volley-ball, amateur de photographie...un contemplatif !",
+        'descriptif' => "Voici Andoniaina Tombozaza, mais vous pouvez l'appeler Ando. <br> Amateur de photographie, il aime capturer la beauté du monde autour de lui. <br> Passionné de volley-ball, il passe souvent son temps libre sur le terrain. <br> Il adore aussi dessiner  prenant toujours son carnet de croquis avec lui lord de ses balades. <br> Il aime également faire des sorties à vélo dans les rues de Paris. <br> Un passionné aux multiples talents, Ando profite de chaque instant.",
     ],
     6 => [
         'id' => 6,
         'photo' => 'public/images/Guillaume.png',
         'prenom' => 'Guillaume',
         'age' => '21',
-        'descriptif' => "Moi c'est Guillaume, mais appelez moi Guigui ou Guigz :)) Je suis un ancien (21 ans eh ouais, le fossile 😅) mais on va plutôt dire que j'ai de l'expérience ! Toujours prêt à rendre service, j'ai 2 passions dans la vie : la musique (👉 va streamer mon groupe sur toutes les plateformes) et les sciences de l'environnement (surtout la chimie, mais curieux de tout 🌱💧🧪). Cette année, j'ai tout fait pour vous régaler avec notre Golden Ale au poivre de Sichuan, alors j'espère que vous allez la kiffer.",
+        'descriptif' => "Hello, moi c'est Guillaume, mais appelez moi Guigui ou Guigz :)) <br> Je suis un ancien (21 ans eh ouais, le fossile 😅) mais on va plutôt dire que j'ai de l'expérience ! <br> Toujours prêt à rendre service, j'ai 2 passions dans la vie : la musique (👉 va streamer mon groupe <br> sur toutes les plateformes) et les sciences de l'environnement (surtout la chimie, <br> mais curieux de tout 🌱💧🧪). Cette année, j'ai tout fait pour vous régaler avec notre Golden Ale <br> au poivre de Sichuan, alors j'espère que vous allez la kiffer.",
     ],
     7 => [
         'id' => 7,
         'photo' => 'public/images/Lou.png',
         'prenom' => 'Lou',
         'age' => '19',
-        'descriptif' => "Je suis en BUT MMI car j’ai toujours voulu en apprendre plus sur le codage, le son, le graphisme et tous les aspects de la formation.",
+        'descriptif' => "Hello, 
+        Moi c’est Lou j’ai 19 ans, grande passionnée de nature je suis incollable sur tout les animaux, curieuse je m’intéresse à la chimie et à la biologie ce qui me permet de faire plein de découvertes. Cette curiosité m’a amené sur le chemin de la bière …
+        C’est pourquoi je suis heureuse de vous présenter nos bières conçues dans un univers rêveurs aux goûts fantaisistes et aux animaux fantastiques ✨",
     ],
     8 => [
         'id' => 8,
         'photo' => 'public/images/Maïna.png',
         'prenom' => 'Maïna',
         'age' => '19',
-        'descriptif' => "Je suis un fan ardu de jeux vidéo surtout de RPG. J’aime découvrir des histoires cachées ou complexes. Je suis en BUT MMI car j’ai toujours voulu en apprendre plus sur le codage, le son, le graphisme et tous les aspects de la formation.",
+        'descriptif' => "Salut,
+        Je m'appelle Maïna. Je passe mon temps à rigoler, je suis un vrai clown. Je suis curieuse et toujours motivée pour apprendre de nouvelles choses 😊. Me lancer dans ce projet m'a permis de découvrir l'univers de la bière, qui est un domaine beaucoup plus riche que ce que l'on pense. Je vous laisse découvrir tout ça grâce à notre marque Infinité ✨.",
     ],
     9 => [
         'id' => 9,
         'photo' => 'public/images/Ruben.png',
         'prenom' => 'Ruben',
         'age' => '19',
-        'descriptif' => "Je suis un fan ardu de jeux vidéo surtout de RPG. J’aime découvrir des histoires cachées ou complexes. Je suis en BUT MMI car j’ai toujours voulu en apprendre plus sur le codage, le son, le graphisme et tous les aspects de la formation.",
+        'descriptif' => "Salut, je m'appelle Ruben, je suis très tête en l'air mais je ne le fais pas exprès 😅
+        J'ai actuellement 19 ans, je suis très curieux et j'adore faire des découvertes 🔎 
+        Je suis toujours à la recherche de nouvelles aventures parce que je cherche constamment à en apprendre plus, et justement, la bière que nous avons créée m'a permis de découvrir le monde qui l'entoure 🍺
+        Notre Souffle Sichuanais a été confectionnée rien que pour vous, alors à vos verres !",
     ],
 ];
 ?>
